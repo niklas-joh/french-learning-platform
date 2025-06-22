8332e99d9c74e42a562818f87849ffb429e4f4e8
Niklas Johansson <15849048+niklas-joh@users.noreply.github.com>
2025-06-22 09:50:31 +0200

    Revert "chore(cleanup): Remove duplicate knexfile from src"
    This reverts commit 4274b8b7e184593cc1556ba3ab56df28be46069b.

---
+++ server/src/knexfile.ts
@@ -0,0 +1,37 @@
+import type { Knex } from 'knex';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.resolve(__dirname, '..', '..', 'database', 'french_learning.db'),
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, '..', '..', 'database', 'migrations'),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, '..', '..', 'database', 'seeds'),
+    },
+  },
+
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, '..', '..', 'database', 'migrations'),
+    },
+    seeds: {
+      directory: path.resolve(__dirname, '..', '..', 'database', 'seeds'),
+    },
+  },
+};
+
+export default config;
