c333460db2c82d5856b6d44654bb223b7b71a612
Niklas Johansson <15849048+niklas-joh@users.noreply.github.com>
2025-09-16 18:54:55 +0200

    refactor(storybook): streamline Storybook configuration by removing unused story paths
    - Removed outdated story paths for MDX and general stories from the Storybook configuration in main.js.
    - Updated the configuration to focus solely on client-specific story files, enhancing clarity and maintainability.
    
    This change simplifies the Storybook setup, ensuring it only includes relevant stories for the current project structure.

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,8 +13,6 @@ function getAbsolutePath(value) {
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
     "../client/src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
